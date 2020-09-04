40af314d8c47db9dfb6db6575d584ba0e036eda4
Data Intern <dataintern@RSO-FVHYGEWWJ1WK.local>
2020-09-04 12:29:53 -0700

    create the table before running script

---
+++ SeeSaw_Student_Activity.sql
@@ -0,0 +1,12 @@
+CREATE TABLE custom.SeeSaw_Student_Activity
+(
+    School_Name VARCHAR(MAX),
+    Student_Name VARCHAR(MAX),
+    Student_ID VARCHAR(MAX),
+    Grade_Level VARCHAR(MAX),
+    Active_Date DATETIME,
+    WasActive BIT,
+    Last_Active_Date VARCHAR(MAX),
+    Date_Uploaded DATE,
+    Link_to_Student_Portfolio VARCHAR(MAX)
+)
